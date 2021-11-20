8bd096f2eecc7729e1e880618ef8eb897f5c8dfc
Joao Araujo <joao.aaraujo@hotmail.com>
2021-11-20 16:29:28 -0300

    Create desafio05.py

---
+++ desafio05.py
@@ -0,0 +1,21 @@
+#1
+n = int(input('Digite un número: '))
+a = n - 1
+s = n + 1
+print('Analisandon o valor {}, o seu antecessor é {}, e seu sucessor é {}'.format(n , a, s))
+print('Analisandon o valor {}, o seu antecessor é {}, e seu sucessor é {}'.format(n , (n-1), (n+1)))
+#2
+
+#3
+
+#4
+
+#5
+
+#6
+
+#7
+
+#8
+
+#9

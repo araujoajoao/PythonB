dd074205b8e0fcb54d43fff33a5107135e2f4cc9
Joao Araujo <joao.aaraujo@hotmail.com>
2021-11-19 21:54:57 -0300

    update

--- Desafio04.py
+++ Desafio04.py
@@ -4,7 +4,7 @@ s = n1 + n2
 print('A soma vale:', s)
 print('A soma entre', n1 ,'e' , n2 , 'vale:', s)
 print('A soma entre {} e {} vale: {}' .format(n1 , n2 , s))
-
+#
 n = input('Digite um valor:')
 print(type(n))
 n = input('Digite um valor:')
@@ -14,4 +14,4 @@ print(n.isalpha())
 n = input('Digite um valor:')
 print(n.isdecimal())
 n = input('Digite um valor:')
-print(n.isnumeric))
+print(n.isnumeric)
